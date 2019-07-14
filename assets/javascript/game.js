de5ed0e7de6f98c02e498d0b16122a01049529d2
Ty Nelson <tynelson48@yahoo.com>
2019-07-14 14:40:46 -0500

    starting function finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,33 +12,60 @@ var animals = [
     "tiger",
     "lion",
 ];
-var choice = animals[Math.floor(Math.random() * animals.length)];
+var choice;
 
 // variable to hold empty array that chosen world will be added to.
 var answerArray = [];
-
-
-
-// for loop to set "_" for each character is chosen word
-for (var i = 0; i < choice.length; i++) {
-    answerArray[i] = "_";
-}
+var answer;
 
 
 // variable to keep track of letters remaining to be guessed.
-var remaining = choice.length;
+var remaining;
 
 // variable for wins
 var wins = 0;
 
 // variable for letters guessed.
-var lettersGuessed = " ";
+var lettersGuessed = "";
 
 // variable to hold instructions.
-var instructions = " ";
+var instructions = "Guess A Letter!";
 
 // variable to guesses remaining and set it to 8.
 var guessRemaining = 8;
+
+
+
+// FUNCTIONs
+
+function chooseWord() {
+    choice = animals[Math.floor(Math.random() * animals.length)];
+    remaining = choice.length;
+
+    for (var i = 0; i < choice.length; i++) {
+        answerArray[i] = "_";
+    }
+
+    document.querySelector("#the-word").innerHTML = answerArray;
+    document.querySelector("#letters-guessed").innerHTML = "Letters Guessed: " + lettersGuessed;
+    document.querySelector("#guesses-remaining").innerHTML = "Guesses Remaining: " + guessRemaining;
+    document.querySelector("#hide").innerHTML = " ";
+    document.querySelector("#instruction").innerHTML = instructions;
+}
+
+function updateScore() {
+    document.querySelector("#wins").innerHTML = "Wins: " + wins;
+}
+
+
+document.onkeyup = function(event) {
+    chooseWord();
+    updateScore();
+}
+
+
+
+
 
 
 
